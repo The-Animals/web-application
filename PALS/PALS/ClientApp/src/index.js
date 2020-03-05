f75f0686f448778f62171da11ab7e39a990bed88
Vishal Patel <vppatel@ualberta.ca>
2020-03-04 18:15:40 -0700

    Added redux, working on incorporating it into project.

--- PALS/PALS/ClientApp/src/index.js
+++ PALS/PALS/ClientApp/src/index.js
@@ -5,12 +5,20 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
-    <App />
+        <Provider store={store}>
+            <App />
+        </Provider>,
   </BrowserRouter>,
   rootElement);
 
