bd897a52bde772704f48f1b33bb1a1833467d7de
Vishal Patel <vppatel@ualberta.ca>
2020-03-13 21:05:20 -0600

    Removed default "weather forcast" page.

--- PALS/PALS/ClientApp/src/components/NavMenu.js
+++ PALS/PALS/ClientApp/src/components/NavMenu.js
@@ -41,9 +41,6 @@ export class NavMenu extends Component {
                     <NavItem>
                       <NavLink tag={Link} className="text-dark" to="/search">Search</NavLink>
                     </NavItem>
-                    <NavItem>
-                      <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                    </NavItem>
                   </ul>
                 </Collapse>
           </Container>
