b5a8b18d2fe6abc126c900e389857c26ea1ab469
Jori Romans <jromans@ualberta.ca>
2020-03-21 23:08:22 -0600

    Added table pagination, fixed api calls, and changed width to match that of MLA Profile card

--- PALS/PALS/ClientApp/src/components/MLASummariesTable.js
+++ PALS/PALS/ClientApp/src/components/MLASummariesTable.js
@@ -9,6 +9,14 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import TablePagination from '@material-ui/core/TablePagination';
+import clsx from 'clsx';
 
 import { getData } from '../services/AjaxService.js';
 
@@ -18,7 +26,7 @@ const mapStateToProps = state => {
 
 const useStyles = makeStyles({
     table: {
-        minWidth: 525,
+        maxWidth: 525,
     },
 });
 
@@ -30,32 +38,60 @@ function MLASummariesTable(props) {
     const [MLASummaries, setMLASummaries] = useState([]);
     useEffect(() => {
         async function getMLASummaries() {
-            const MLASummaries = await getData('api/GetSummary', { RidingID: mlaId });
-            setMLASummaries(MLASummaries);            
+            const MLASummaries = await getData('api/summary/mla/' + mlaId + '/500');
+            setMLASummaries(MLASummaries);
+            console.log(MLASummaries);
         }
         getMLASummaries();
     }, [mlaId]);
 
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = event => {
+      setRowsPerPage(parseInt(event.target.value, 10));
+      setPage(0);
+    };
+
     return (
-        <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                    <TableRow><TableCell>Summaries</TableCell></TableRow>
-                </TableHead>
-                <TableBody>
-                    {MLASummaries.map(row => (
-                        <TableRow key={row.summaryRank}>
-                            <TableCell align="left">
-                                {row.summaryRank}
-                            </TableCell>
-                            <TableCell align="left">
-                                {row.text}
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Paper className={classes.paper}>
+          <TableContainer component={Paper}>
+              <Table className={classes.table} aria-label="simple table">
+                  <TableHead>
+                      <TableRow>
+                        <TableCell align='center' colspan='2'>
+                          Summaries
+                        </TableCell>
+                      </TableRow>
+                  </TableHead>
+                  <TableBody>
+                      {MLASummaries.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
+                          <TableRow key={row.mlaRank}>
+                              <TableCell align="left">
+                                  {row.mlaRank}
+                              </TableCell>
+                              <TableCell align="left">
+                                  {row.text}
+                              </TableCell>
+                          </TableRow>
+                      ))}
+                  </TableBody>
+              </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[1, 2, 3, 4, 5]}
+            component="div"
+            count={MLASummaries.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+        </Paper>
     );
 }
 
