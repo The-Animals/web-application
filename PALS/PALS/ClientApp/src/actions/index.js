9a18e51ee34b98399a534175df935a1b20c5d5cc
Vishal Patel <vppatel@ualberta.ca>
2020-03-22 19:24:41 -0600

    [chore] Remove search results from redux store.

--- PALS/PALS/ClientApp/src/actions/index.js
+++ PALS/PALS/ClientApp/src/actions/index.js
@@ -1,16 +1,9 @@
 ﻿import { VIEW_MLA } from "../constants/action-types.js";
-import { UPDATE_RESULTS } from "../constants/action-types.js";
 import { UPDATE_SUMMARY_FILTER } from "../constants/action-types.js";
 import { UPDATE_ALL_SUMMARIES } from "../constants/action-types.js";
 
 export function viewMLA(mlaId) {
     return { type: VIEW_MLA, mlaId }
-};
-
-// TODO: Remove search results, instead use summary view for
-// currently filtered summaries.
-export function updateSearchResults(results) {
-    return { type: UPDATE_RESULTS , results }
 };
 
 export function updateSummaryFilter(filter) {
--- PALS/PALS/ClientApp/src/reducers/index.js
+++ PALS/PALS/ClientApp/src/reducers/index.js
@@ -1,11 +1,9 @@
 ﻿import { VIEW_MLA } from "../constants/action-types.js";
-import { UPDATE_RESULTS } from "../constants/action-types.js";
 import { UPDATE_SUMMARY_FILTER } from "../constants/action-types.js";
 import { UPDATE_ALL_SUMMARIES } from "../constants/action-types.js";
 
 const initialState = {
     mla: {},
-    searchResults: [],
     summaries: [],
     summaryFilter:
     {
@@ -22,8 +20,6 @@ function rootReducer(state = initialState, action) {
     switch (action.type) {
         case VIEW_MLA:
             return { ...state, mla: action.mlaId }
-        case UPDATE_RESULTS:
-            return { ...state, searchResults: action.results }
         case UPDATE_ALL_SUMMARIES:
             return { ...state, summaries: action.summaries }
         case UPDATE_SUMMARY_FILTER:
