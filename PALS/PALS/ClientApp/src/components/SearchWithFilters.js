4c197c8da524fcb4d37c9c952ffb983750e9ac0b
Vishal Patel <vppatel@ualberta.ca>
2020-03-23 15:56:09 -0600

    [feat] Filtering mlas based on party selection.
    If an MLA is selected that is not in the current party, it will change the party filter back to ALL.

--- PALS/PALS/ClientApp/src/components/SearchWithFilters.js
+++ PALS/PALS/ClientApp/src/components/SearchWithFilters.js
@@ -29,11 +29,31 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
+function filterMLAsByParty(mlas, party) {    
+
+    if (party === "ALL") return mlas;
+
+    return mlas.filter(function (mla) {
+        // If mla party matches filter or mla has no associated party.
+        return mla.party === party || !mla.party;
+    });
+}
+
+// TODO: Maybe it would be better for the endpoint to return a dictionary.
+function mapMlasToObject(mlas) {
+
+    return mlas.reduce(function (map, obj) {
+        map[obj.id] = obj;
+        return map;
+    }, {});
+
+}
+
 function SearchWithFilters(props) {
     const classes = useStyles();
-    
+
     const [mla, setMla] = useState(0);
-    const handleMlaChange = event => {        
+    const handleMlaChange = event => {           
         setMla(event.target.value);
     };
 
@@ -54,24 +74,30 @@ function SearchWithFilters(props) {
 
     const [party, setParty] = useState("ALL");
     const handlePartyChange = (event, newParty) => {
+
         if (newParty !== null) {
             setParty(newParty);            
         }
+
+        var mlas = mapMlasToObject(props.mlas);
+        var selectedMLA = mlas[mla];  
+
+        if (newParty !== "ALL" && selectedMLA.party !== newParty) {            
+            setMla(props.mlas[0].id);
+        }
     };
+   
+    var filteredMLAs = filterMLAsByParty(props.mlas, party);
 
     useEffect(() => {
-        handleSearch();
-    }, [query, mla, party, startDate, endDate]);
-
-    const handleSearch = function () {
         props.updateSummaryFilter({
             mlaId: mla,
             caucus: party,
             startDate: startDate,
             endDate: endDate,
             query: query,
-        });   
-    } 
+        });     
+    }, [query, mla, party, startDate, endDate]);
 
     // TODO: Replace native date pickers with material-ui/pickers
     return (
@@ -106,7 +132,7 @@ function SearchWithFilters(props) {
                 onChange={handleMlaChange}
                 helperText="Please select a riding"
             >
-                {props.mlas.map(option => (
+                {filteredMLAs.map(option => (
                     <MenuItem key={option.id} value={option.id}>
                         {option.riding}
                     </MenuItem>
@@ -121,7 +147,7 @@ function SearchWithFilters(props) {
                 onChange={handleMlaChange}
                 helperText="Please select an MLA"
             >
-                {props.mlas.map(option => (
+                {filteredMLAs.map(option => (
                     <MenuItem key={option.id} value={option.id}>
                         {option.name}
                     </MenuItem>
