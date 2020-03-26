eca738fbb13112ab048d6b6daa377cc9f6d2bb80
Vishal Patel <vppatel@ualberta.ca>
2020-03-25 22:57:36 -0600

    Search table ordering (#8)
    * [feat] Implemented pagination for search table rendering.
    
    * [partially-complete] Requests data in chunks of 1000 until 10000 if no results displayed.
    
    Now requests data in chunks of 1000 if the filters show no data until we reach 10000 entries. This can likely be improved by using react-thunk.
    
    * [feat] Used redux-thunk to fetch summaries.
    
    * [chore] Moved fetch summaries action to its own file
    
    * [feat] Now shows loading and no results found message.
    
    Shows loading message while querying more chunks, and shows no results found if it reacts the max limit and still has no results to show.
    
    Also should only load one chunk at a time.
    
    * [feat] Implement ordering by mla rank and date
    
    * [bug-fix] Search page now displays results the first time
    
    Fixed bug where search page was not displaying the results the first time it was opened.
    
    * [chore] Cleanup some warnings
    
    * [feat] Implement ordering by party rank

--- PALS/PALS/ClientApp/src/components/SearchTable.js
+++ PALS/PALS/ClientApp/src/components/SearchTable.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
+import PropTypes from 'prop-types';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -8,6 +9,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import TablePagination from '@material-ui/core/TablePagination';
 import Paper from '@material-ui/core/Paper';
 
@@ -43,7 +45,122 @@ const useStyles = makeStyles({
     table: {
         minWidth: 650,
     },
+    visuallyHidden: {
+        border: 0,
+        clip: 'rect(0 0 0 0)',
+        height: 1,
+        margin: -1,
+        overflow: 'hidden',
+        padding: 0,
+        position: 'absolute',
+        top: 20,
+        width: 1,
+    },
 });
+
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function descendingDateComparator(a, b, orderBy) {
+    const dateA = new Date(a[orderBy]);
+    const dateB = new Date(b[orderBy]);
+
+    if (dateB < dateA) {
+        return -1;
+    }
+    if (dateB > dateA) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy) {
+
+    // TODO: Can be improved by adding a "type" field to the header,
+    // which is passed to getComparator() which performs a select-case
+    // depending on type.
+    if (orderBy === "date") {
+        return order === 'desc'
+            ? (a, b) => descendingDateComparator(a, b, orderBy)
+            : (a, b) => -descendingDateComparator(a, b, orderBy);
+    }
+
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map(el => el[0]);
+}
+
+const headCells = [
+    { id: 'person', sortable: false, alignRight: false, disablePadding: false, label: 'Person' },
+    { id: 'mlaRank', sortable: true, alignRight: false, disablePadding: false, label: 'MLA Rank' },
+    { id: 'partyRank', sortable: true, alignRight: false, disablePadding: false, label: 'Party Rank' },
+    { id: 'documentDate', sortable: true, alignRight: false, disablePadding: false, label: 'Date' },
+    { id: 'summary', sortable: false, alignRight: false, disablePadding: false, label: 'Summary' },
+];
+
+function EnhancedTableHead(props) {
+    const { classes, order, orderBy, onRequestSort } = props;
+    const createSortHandler = property => event => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <TableRow>
+                {headCells.map(headCell => (
+                    headCell.sortable ?
+                        <TableCell
+                            key={headCell.id}
+                            align={headCell.alignRight ? 'right' : 'left'}
+                            padding={headCell.disablePadding ? 'none' : 'default'}
+                            sortDirection={orderBy === headCell.id ? order : false}
+                        >
+                            <TableSortLabel
+                                active={orderBy === headCell.id}
+                                direction={orderBy === headCell.id ? order : 'asc'}
+                                onClick={createSortHandler(headCell.id)}
+                            >
+                                {headCell.label}
+                                {orderBy === headCell.id ? (
+                                    <span className={classes.visuallyHidden}>
+                                        {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                    </span>
+                                ) : null}
+                            </TableSortLabel>
+                        </TableCell>
+                        :
+                        <TableCell key={headCell.id}>
+                            {headCell.label}
+                        </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    classes: PropTypes.object.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+};
 
 const generateKey = (result) => {
     return result.mlaRank + '_' + result.mlaId;
@@ -54,12 +171,12 @@ function processSummaryFilter(filter, summaries) {
 
         // Sort mlaIds
         if (filter.mlaId &&
-            (filter.mlaId != summary.mlaId)) return false;
+            (filter.mlaId !== summary.mlaId)) return false;
 
         // Sort caucus
         if (filter.caucus &&
-            (filter.caucus != "ALL" &&
-             filter.caucus != summary.caucus)) return false;
+            (filter.caucus !== "ALL" &&
+             filter.caucus !== summary.caucus)) return false;
 
         // Sort dates
         const summaryDate = new Date(summary.documentDate);
@@ -78,14 +195,14 @@ function processSummaryFilter(filter, summaries) {
             const query = filter.query.toLowerCase();
             const summaryText = summary.text.toLowerCase();
             if (!summaryText.includes(query)) return false;
-        }            
+        }
 
         return true;
 
-    });    
+    });
 }
 
-function generateMlaRows(filteredSummaries, loading, sliceStart, sliceEnd) {
+function generateMlaRows(filteredSummaries, loading, sliceStart, sliceEnd, order, orderBy) {
 
     if (loading) {
         return (
@@ -97,7 +214,7 @@ function generateMlaRows(filteredSummaries, loading, sliceStart, sliceEnd) {
         );
     }
 
-    if (filteredSummaries.length == 0) {
+    if (filteredSummaries.length === 0) {
         return (
             <TableRow>
                 <TableCell align="left">
@@ -107,13 +224,22 @@ function generateMlaRows(filteredSummaries, loading, sliceStart, sliceEnd) {
         );
     }
 
-    return filteredSummaries
+    return stableSort(filteredSummaries, getComparator(order, orderBy))
         .slice(sliceStart, sliceEnd)
         .map(result =>
             <TableRow key={generateKey(result)}>
                 <PersonTableCell component="th" scope="row">
-                    <img src={MLA} width="120px" height="150px" />
+                    <img src={MLA} alt={result.name} width="120px" height="150px" />
                 </PersonTableCell>
+                <TableCell>
+                    {result.mlaRank}
+                </TableCell>
+                <TableCell>
+                    {result.partyRank}
+                </TableCell>
+                <TableCell>
+                    {new Date(result.documentDate).toLocaleDateString('en-US')}
+                </TableCell>
                 <TableCell align="left">
                     {result.text}
                 </TableCell>
@@ -124,15 +250,25 @@ function generateMlaRows(filteredSummaries, loading, sliceStart, sliceEnd) {
 function SearchTable(props) {
     const classes = useStyles();
 
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('rank');
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+        console.log("Sort me, ", order, orderBy);
+    };
+
     const summaries = props.summaries;
     const filteredSummaries = processSummaryFilter(props.summaryFilter, summaries);
 
     if (summaries.length > 0 &&
-        filteredSummaries.length == 0 &&
+        filteredSummaries.length === 0 &&
         !props.loading) {
 
-        props.updateSummaryOffset(1001);        
-    }        
+        props.updateSummaryOffset(1001);
+    }
 
     const [page, setPage] = React.useState(0);
     const handleChangePage = (event, newPage) => {
@@ -149,7 +285,9 @@ function SearchTable(props) {
         filteredSummaries,
         props.loading,
         page * rowsPerPage,
-        page * rowsPerPage + rowsPerPage);
+        page * rowsPerPage + rowsPerPage,
+        order,
+        orderBy);
 
     return (
         <Paper>
@@ -164,12 +302,12 @@ function SearchTable(props) {
             />
             <TableContainer>
                 <Table className={classes.table} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Person</TableCell>
-                            <TableCell align="left">Summary</TableCell>                    
-                        </TableRow>
-                    </TableHead>
+                    <EnhancedTableHead
+                        classes={classes}
+                        order={order}
+                        orderBy={orderBy}
+                        onRequestSort={handleRequestSort}
+                    />
                     <TableBody>
                         {mlaRows}
                     </TableBody>
