578f4e6b5d3d8ae72c3ecccb9add11b3fcb0a6aa
zdrever <zdrever@ualberta.ca>
2020-03-28 15:53:04 -0600

    [feat] select random mla to render on intial load

--- PALS/PALS/ClientApp/src/components/Home.js
+++ PALS/PALS/ClientApp/src/components/Home.js
@@ -4,17 +4,23 @@ import { connect } from "react-redux";
 import { Summary } from './Summary';
 import MapBox from './Map';
 import InteractiveGraph from './InteractiveGraph';
-import { fetchMlas } from "../actions/mlaListActions";
+import { fetchMlas, mlaSelected } from "../actions/mlaListActions";
+import { fetchMlaSummaries } from '../actions/mlaSummaryActions';
+import { fetchMlaParticipation } from '../actions/mlaParticipationActions';
 
 const mapStateToProps = state => {
     return {
-        mlas: state.mlas
+        mlas: state.mlas,
+        mla: state.mla
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        fetchMlas: () => dispatch(fetchMlas())
+        fetchMlas: () => dispatch(fetchMlas()),
+        fetchMlaSummaries: mlaId => dispatch(fetchMlaSummaries(mlaId)),
+        fetchMlaParticipation: mlaId => dispatch(fetchMlaParticipation(mlaId)),
+        mlaSelected: mla => dispatch(mlaSelected(mla))
     };
 }
 
@@ -23,7 +29,15 @@ class Home extends Component {
 
     componentDidMount() {
         this.props.fetchMlas();
-        
+    }
+
+    componentWillReceiveProps(props) {
+        if (Object.keys(this.props.mla).length === 0) {
+            const mla = props.mlas.slice(1, props.mlas.length)[Math.floor(Math.random() * props.mlas.length)];
+            this.props.fetchMlaSummaries(mla.id);
+            this.props.fetchMlaParticipation(mla.id);
+            this.props.mlaSelected(mla);
+        }
     }
 
     render() {
