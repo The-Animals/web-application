65147a249d5b5ce1ab69fda37f7208fd9c4c6198
Vishal Patel <vppatel@ualberta.ca>
2020-04-02 16:04:15 -0600

    [bug-fix] Now aborting previous MLA summary requests as well.

--- PALS/PALS/ClientApp/src/actions/mlaSummaryActions.js
+++ PALS/PALS/ClientApp/src/actions/mlaSummaryActions.js
@@ -2,6 +2,7 @@ import {
     FETCH_MLA_SUMMARIES_BEGIN, 
     FETCH_MLA_SUMMARIES_SUCCESS, 
     FETCH_MLA_SUMMARIES_ERROR,
+    FETCH_MLA_SUMMARIES_ABORT,
     SET_MLA_SUMMARY_DATE_FILTER
 } from '../constants/mlaSummariesActionTypes.js';
 
@@ -19,22 +20,48 @@ export const fetchMlaSummariesError = error => ({
     payload: { error }
 });
 
+export const fetchMlaSummariesAbort = () => ({
+    type: FETCH_MLA_SUMMARIES_ABORT
+});
+
 export const setMlaSummaryDateFilter = mlaSummaryDateFilter => ({
     type: SET_MLA_SUMMARY_DATE_FILTER, 
     payload: { mlaSummaryDateFilter }
 })
 
+function abortableFetch(signal, dispatch, mlaId) {
+    dispatch(fetchMlaSummariesBegin());
+    return fetch('api/summary/' + mlaId, { signal })
+        .then(handleErrors)
+        .then(res => res.json())
+        .then(json => {
+            dispatch(fetchMlaSummariesSuccess(json));
+            return json;
+        })
+        .catch(error => dispatch(fetchMlaSummariesError(error)));
+}
+
 export function fetchMlaSummaries(mlaId) {
-    return (dispatch) => {
-        dispatch(fetchMlaSummariesBegin());
-        return fetch('api/summary/' + mlaId)
-            .then(handleErrors)
-            .then(res => res.json())
-            .then(json => {
-                dispatch(fetchMlaSummariesSuccess(json));
-                return json;
-            })
-            .catch(error => dispatch(fetchMlaSummariesError(error)));
+    return (dispatch, getState) => {
+        
+        // Cancel previous request, and create another one.
+        if (getState().mlaSummariesLoading) {
+
+            setTimeout(() => {
+
+                getState().mlaSummariesAbort.abort();
+                dispatch(fetchMlaSummariesAbort());
+
+                const { signal } = getState().mlaSummariesAbort;
+                return abortableFetch(signal, dispatch, mlaId);
+
+            }, 100);
+        } else {
+
+            const { signal } = getState().mlaSummariesAbort;
+            return abortableFetch(signal, dispatch, mlaId);
+        }        
+
     };
 }
 
